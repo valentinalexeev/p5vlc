83c8bef53c8dbfc8b66bdc089538d69afb0afae4
Valentin Alexeev <valentin.alekseev@gmail.com>
2006-02-22 19:30:05 +0000

    Prepare everything to export HTML documentation to SF.
    git-svn-id: https://svn.code.sf.net/p/p5vlc/code/trunk@5 31dd1ae6-650d-0410-9999-b38704b9252d

--- doc/mkhtmldoc.sh
+++ doc/mkhtmldoc.sh
@@ -2,5 +2,5 @@
 mkdir -p html/VLC/Transport
 mkdir -p html/VLC/Helper/Stream/Access
 mkdir -p html/VLC/Helper/Stream/Mux
-find ../lib -type f | sed -e 's/\(.*\)/pod2html --htmlroot=`pwd`\/html --infile=\1 \1/' -e 's/m \.\.\/lib/m --outfile=html/' -e 's/pm$/html/' | /bin/sh -
+find ../lib -type f | grep -v -e svn | sed -e 's/\(.*\)/pod2html --infile=\1 \1/' -e 's/m \.\.\/lib/m --outfile=html/' -e 's/pm$/html/' | /bin/sh -
 rm *tmp
